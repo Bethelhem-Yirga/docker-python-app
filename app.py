11413199a0eedff1aea5392457abc9209a3ec42f
Bethelhem-Yirga <bethelhemyirga3@gmail.com>
2026-07-03 15:16:17 +0800

    linting

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Welcome to my updated Docker app! 🚀 This is version 2.01! trying volume mountswwwww compose"
 
-@app.route('/health')
+@app.route("/")
+def hello():
+    return (
+        "Welcome to my updated Docker app! 🚀 "
+        "This is version 2.01! trying volume mountswwwww compose"
+    )
+
+
+@app.route("/health")
 def health():
     return "OK", 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
